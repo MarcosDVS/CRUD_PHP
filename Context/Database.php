eec7f971acaaa0f0e3dfdc84b25dcaa0d6e3028b
El Bronco <2020-0288@uteco.edu.do>
2024-12-07 16:29:32 -0400

    Explaining the connection to the database.

--- Context/Database.php
+++ Context/Database.php
@@ -1,20 +1,28 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "ejemplo_crud";
-    private $username = "root";
-    private $password = "";
+    private $host = "localhost"; //Direccion ip de tu servidor
+    private $db_name = "ejemplo_crud"; //Nombre de tu database
+    private $username = "root"; //Nombre de usuario en el servidor que aloja tu database
+    private $password = ""; //Password del servidor que aloja tu database
     public $conn;
 
+    /**
+     * Establece una conexión a la base de datos utilizando PDO.
+     *
+     * @return PDO|null Devuelve la conexión PDO o null si falla.
+     */
     public function getConnection() {
-        $this->conn = null;
+        $this->conn = null; // Inicializa la conexión como nula
         try {
+            // Crea una nueva conexión PDO (PHP Data Objects)
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            // Establece el modo de error de PDO a excepción
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) {
+            // Captura cualquier excepción y muestra un mensaje de error
             echo "Error de conexión: " . $exception->getMessage();
         }
-        return $this->conn;
+        return $this->conn; // Devuelve la conexión
     }
 }
 ?>
