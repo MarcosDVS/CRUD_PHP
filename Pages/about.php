1f0923afbb6affe06f3ff5269733bc123cb4e1fb
El Bronco <2020-0288@uteco.edu.do>
2024-12-07 17:01:41 -0400

    Adding picture

--- Pages/about.php
+++ Pages/about.php
@@ -9,5 +9,12 @@ require_once "../Shared/Header.php";
 
 <h2>Contacto</h2>
 <p>Si tienes alguna pregunta, no dudes en contactarnos a través de nuestro correo electrónico: <a href="mailto:info@tuempresa.com">info@tuempresa.com</a>.</p>
+<div class="row">
+    <div class="col text-left">
+        <img src="../Shared/img/Bojack.jpg" alt="Bojack" 
+        style="width: 150px; border-radius: 15px;" />
+        <p style="font-weight: bold; font-size: 1.2em; color: #333;">(Horse Man)</p>
+    </div>
+</div>
 
 <?php require_once "../Shared/Footer.php"; ?>
