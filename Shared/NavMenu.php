7dcfbb331559139d482e1105ac834395fe6bec0b
El Bronco <2020-0288@uteco.edu.do>
2024-12-13 09:48:31 -0400

    Just I changed the option Index for Productos

--- Shared/NavMenu.php
+++ Shared/NavMenu.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>" aria-current="page" href="../Pages/index.php">Index</a>
+                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>" aria-current="page" href="../Pages/index.php">Productos</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'Clientes.php' ? 'active' : ''; ?>" aria-current="page" href="../Pages/Clientes.php">Clientes</a>
